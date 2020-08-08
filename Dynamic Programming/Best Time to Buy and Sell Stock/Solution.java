7feb66a9905bbcfc6724a42a53cf5b5e5e974ed3
CHIRU-17BCI0152 <44204191+CHIRU3223@users.noreply.github.com>
2020-08-08 22:54:46 +0530

    O(n)

---
+++ Dynamic Programming/Best Time to Buy and Sell Stock/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0,max=0,min=Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<min)
+                min=prices[i];
+            else if(prices[i]-min>profit)
+                profit=prices[i]-min;
+            
+        }
+        return profit;
+    }
+}
